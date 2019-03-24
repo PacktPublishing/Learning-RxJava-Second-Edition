dc1c622fd1beff86fc88e5bdc0b396d93097e717
Nick Samoylov <nick.samoylov@ctl.io>
2019-03-24 12:14:41 -0600

    Add examples of chapter 1

---
+++ Chapter01/Ch1_1.java
@@ -0,0 +1,9 @@
+import io.reactivex.Observable;
+
+public class Ch1_1 {
+    public static void main(String[] args) {
+        Observable<String> myStrings =
+                Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
+        myStrings.subscribe(s -> System.out.println(s));
+    }
+}
