7135ffaf64c8b640eb0f37033e2abb50964a04c2
Nick Samoylov <nick.samoylov@ctl.io>
2019-06-22 17:42:58 -0600

    Examples for chapter 11

---
+++ Chapter11/RxJavaApp1/app/src/test/java/com/packtpub/rxjavaapp/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.packtpub.rxjavaapp;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void lambdaTest() throws Exception {
+        Callable<Integer> callable = () -> 4;
+        System.out.println(callable.call());
+    }
+}
