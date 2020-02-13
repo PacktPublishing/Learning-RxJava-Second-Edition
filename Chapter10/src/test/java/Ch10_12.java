3b7a7aefcd16f537504126a8d496c2fb386d4181
Nick Samoylov <nick.samoylov@ctl.io>
2020-02-12 19:54:11 -0700

    Migrate to version 3

--- Chapter10/src/test/java/Ch10_12.java
+++ Chapter10/src/test/java/Ch10_12.java
@@ -1,6 +1,7 @@
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.observers.TestObserver;
 import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,13 +11,14 @@ public class Ch10_12 {
         //An Observable with 5 one-second emissions
         Observable<Long> source =
                 Observable.interval(1, TimeUnit.SECONDS)
-                          .take(5);
+                          .take(3);
 
         //Declare TestObserver
         TestObserver<Long> testObserver = new TestObserver<>();
 
         //Assert no subscription has occurred yet
         //testObserver.assertNotSubscribed();
+        assertFalse(testObserver.hasSubscription());
 
         //Subscribe TestObserver to source
         source.subscribe(testObserver);
@@ -26,6 +28,11 @@ public class Ch10_12 {
 
         //Block and wait for Observable to terminate
         //testObserver.awaitTerminalEvent();
+        try {
+            testObserver.await(4L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         //Assert TestObserver called onComplete()
         testObserver.assertComplete();
@@ -34,9 +41,9 @@ public class Ch10_12 {
         testObserver.assertNoErrors();
 
         //Assert 5 values were received
-        testObserver.assertValueCount(5);
+        testObserver.assertValueCount(3);
 
         //Assert the received emissions were 0, 1, 2, 3, 4
-        testObserver.assertValues(0L, 1L, 2L, 3L, 4L);
+        testObserver.assertValues(0L, 1L, 2L);
     }
 }
