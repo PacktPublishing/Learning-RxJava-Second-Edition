c842f0a891479b3b5819c7ba575229eb28119d09
Nick Samoylov <nick.samoylov@ctl.io>
2020-02-15 08:44:03 -0700

    A bit of reformating

--- Chapter02/Ch2_14.java
+++ Chapter02/Ch2_14.java
@@ -4,8 +4,7 @@ import io.reactivex.rxjava3.observables.ConnectableObservable;
 public class Ch2_14 {
     public static void main(String[] args) {
         ConnectableObservable<String> source =
-                Observable.just("Alpha", "Beta", "Gamma")
-                        .publish();
+                Observable.just("Alpha", "Beta", "Gamma").publish();
         //Set up observer 1
         source.subscribe(s -> System.out.println("Observer 1: " + s));
         //Set up observer 2
